bf04bb23518e5c6f49a1a19d05881283301cd1bf
kwatch <kwatch@gmail.com>
2023-11-26 07:31:10 +0900

    update 'Rakefile.rb'

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
 
-PROJECT   = "benry-hello"
+PROJECT   = "git-improved"
 RELEASE   = ENV['RELEASE'] || "0.0.0"
-COPYRIGHT = "copyright(c) 2020 kwatch@gmail.com"
+COPYRIGHT = "copyright(c) 2023 kwatch@gmail.com"
 LICENSE   = "MIT License"
 
 #RUBY_VERSIONS = ["3.2", "3.1", "3.0", "2.7", "2.6", "2.5", "2.4", "2.3"]
 
 Dir.glob('./task/*-task.rb').sort.each {|x| require x }
+
+def do_doc()
+  x = PROJECT
+  md2 = "../benry-ruby/docs/md2"
+  sh "#{md2} --md docs/#{x}.mdx > README.md"
+  sh "#{md2}      docs/#{x}.mdx > docs/index.html"
+end
+
+desc "copy '*.task' files"
+task :copytask do
+  sh "cp -a ../benry-ruby/task ."
+end
