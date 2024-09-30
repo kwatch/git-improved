a6cf04028e68fa8936cdc8e8d6048b809a22f591
kwatch <kwatch@gmail.com>
2024-10-01 00:35:13 +0900

    update copyright year

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,7 +3,7 @@
 
 PROJECT   = "git-improved"
 RELEASE   = ENV['RELEASE'] || "0.0.0"
-COPYRIGHT = "copyright(c) 2023 kwatch@gmail.com"
+COPYRIGHT = "copyright(c) 2023-2024 kwatch@gmail.com"
 LICENSE   = "MIT License"
 
 #RUBY_VERSIONS = ["3.2", "3.1", "3.0", "2.7", "2.6", "2.5", "2.4", "2.3"]
